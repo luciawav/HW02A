7c7e591e754e8530bd04c891c7843172a7b3191d
luciawav <144282035+luciawav@users.noreply.github.com>
2023-09-16 17:37:14 -0400

    HW02A
    SQUARE & TRIAN

--- sketch.js
+++ sketch.js
@@ -1,5 +1,20 @@
 function setup() {
+    createCanvas(1200,1200);
+    background(244,248,253);
 }
 
 function draw() {
+    noStroke();
+
+    fill(18,27,32);
+    quad(46, 43, 1140, 69, 1123,1136, 39,1121);
+
+
+    fill(239,225,202);
+    triangle(50,45, 1138,69,783,560);//TOP
+    triangle(1134,75,1127,795,626,795);//RIGHT
+    triangle(44,393,533,386,36,1121);//LEFT
+    triangle(376,627,1113,1135,42,1124);//BOTTOM
+
 }
+
